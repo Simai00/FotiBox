2ed061e47d54fd9819571d8ed42f0c0c5f7f3abd
Simai00 <simon.friedli@gibmit.ch>
2019-10-13 20:56:05 +0200

    Remove unnecessary things

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,21 +1,15 @@
 <template>
     <div>
         <TakeAPhoto/>
-        <HelloWorld/>
-        <Gallery/>
     </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld';
-  import Gallery from '../components/Gallery';
   import TakeAPhoto from '../components/TakeAPhoto';
 
   export default {
     components: {
-      TakeAPhoto,
-      HelloWorld,
-      Gallery
-    },
+      TakeAPhoto
+    }
   };
 </script>
