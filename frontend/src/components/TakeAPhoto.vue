6c122ec7b7c865d09d1b3c7179ef7f6b81985986
Simai00 <simon.friedli@gibmit.ch>
2019-10-13 21:20:51 +0200

    Make CountDown work

--- frontend/src/components/TakeAPhoto.vue
+++ frontend/src/components/TakeAPhoto.vue
@@ -4,31 +4,44 @@
         <v-card-title>FottiBox</v-card-title>
         <v-card-text>
             <v-img src="https://picsum.photos/510/300?random" aspect-ratio="2"></v-img>
-            <div>
+            <div v-if="countDownStarted">
                 <p>{{countDown}}</p>
             </div>
         </v-card-text>
         <v-card-actions>
-            <button v-on:click="countDownTimer">Schiess es Fotti!</button>
+            <v-btn :disabled="countDownStarted" @click="startCountDown()">Schiess es Fotti!</v-btn>
         </v-card-actions>
     </v-card>
 </template>
 <script>
 
   export default {
-    data: () => ({
+    data: function () {
+      return {
+        countDown: Number,
+        countDownStarted: false
+      };
+    },
     methods: {
+      startCountDown () {
+        if (!this.countDownStarted) {
+          this.countDown = 5;
+          this.countDownStarted = true;
+          this.countDownTimer();
+        }
+      },
       countDownTimer () {
         if (this.countDown > 0) {
           setTimeout(() => {
             this.countDown -= 1;
-            this.countDownTimer()
-          }, 1000)
+            this.countDownTimer();
+          }, 1000);
+        } else {
+          this.countDownStarted = false;
         }
       }
     }
-  })
-  }
+  };
 
 
 </script>
