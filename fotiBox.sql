627d5d080fac0fe24841588f968d1efc3d972fc5
Simai00 <simon.friedli@gibmit.ch>
2019-10-18 15:58:23 +0200

    Change image to BW and back

--- fotiBox.sql
+++ fotiBox.sql
@@ -7,6 +7,6 @@ CREATE TABLE `image`
     `id`        int(11)      NOT NULL AUTO_INCREMENT,
     `path`      varchar(100) NOT NULL,
     `createdAt` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `filter`    BOOLEAN      NOT NULL,
+    `bwFilter`  BOOLEAN      NOT NULL,
     PRIMARY KEY (id)
 );
